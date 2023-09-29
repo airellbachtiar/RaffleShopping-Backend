fbf534cd9c21fbc962238fb7debce1a12cac6a9b
airell <i450046@fhict.nl>
2023-09-29 12:53:43 +0200

    Created back-end skeleton

---
+++ RaffleShoppingAPI/RaffleShopping.Services.Customers.Api/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RaffleShopping.Services.Customers.Models;
+
+namespace RaffleShopping.Services.Customers.Api.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class CustomersController : Controller
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel loginModel)
+        {
+            // Replace this with your actual authentication logic
+            if (loginModel.Email == "example@email.com" && loginModel.Password == "examplePassword")
+            {
+                // Authentication successful
+                return Ok(new { Message = "Login successful" });
+            }
+            else
+            {
+                // Authentication failed
+                return Unauthorized(new { Message = "Invalid credentials" });
+            }
+        }
+    }
+}
---
+++ RaffleShoppingAPI/RaffleShopping.Services.RaffleEvents.Api/Controllers/RaffleEventsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RaffleShoppping.Services.RaffleEvents.Dtos;
+
+namespace RaffleShopping.Services.RaffleEvents.Api.Controllers
+{
+    [ApiController]
+    [Route("api/raffle-events")]
+    public class RaffleEventsController : Controller
+    {
+        [HttpGet("get-raffle-event", Name = "GetRaffleEvent")]
+        public List<RaffleEventDto> GetRaffleEvent()
+        {
+            return new List<RaffleEventDto> 
+            {   
+                new RaffleEventDto
+                {
+                    Title = "Test Item 1",
+                    Price = 0
+                } ,
+                new RaffleEventDto
+                {
+                    Title = "Test Item 2",
+                    Price = 3
+                }
+            };
+        }
+    }
+}
