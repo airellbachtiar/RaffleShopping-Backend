6b39d6830d8d1346e1963903bdaee1d6347b2afd
airell <i450046@fhict.nl>
2023-10-23 09:21:34 +0200

    Added test

---
+++ RaffleShopping.Services.Customers.UnitTests/Features/LoggingInCustomer/LoginCustomerTests.cs
@@ -0,0 +1,24 @@
+﻿using RaffleShopping.Services.Customers.Features.LoggingInCustomer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaffleShopping.Services.Customers.UnitTests.Features.LoggingInCustomer
+{
+    public class LoginCustomerTests
+    {
+        LoginCustomer _loginCustomer = new LoginCustomer();
+        
+        [Test]
+        public void CustomerShouldLoginWithValidCredentials()
+        {
+            string email = "a@b.c";
+            string password = "password";
+
+            Assert.IsTrue(_loginCustomer.Login(email, password));
+        }
+    }
+}
