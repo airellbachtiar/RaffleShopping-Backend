3bd280b4795a3c4896892714a561f9de5446b856
airell <i450046@fhict.nl>
2023-10-27 13:56:05 +0200

    Minor fixes

--- RaffleShopping.Services.Customers/Models/Customer.cs
+++ RaffleShopping.Services.Customers/Models/Customer.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace RaffleShopping.Services.Customers.Models
 {
     public class Customer
     {
-        [Key]
-        [Required]
-        public string _Id { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _id { get; set; }
 
         [Required]
         public string Email { get; set; }
