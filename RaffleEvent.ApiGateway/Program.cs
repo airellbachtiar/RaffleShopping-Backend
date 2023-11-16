494d2654ea07121c354bb8e3b96aa3ab61c1195d
airell <i450046@fhict.nl>
2023-11-16 13:32:29 +0100

    Added ocelot.json and Dockerfile

--- RaffleEvent.ApiGateway/Program.cs
+++ RaffleEvent.ApiGateway/Program.cs
@@ -1,11 +1,16 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Provider.Consul;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//Add Ocelot JSON
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration).AddConsul();
 
 var app = builder.Build();
 
